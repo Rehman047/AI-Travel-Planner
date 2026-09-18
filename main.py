74fcc5178e0f0d9214042fecade3120ee1289d40
Abdul Rehman <dragonbhatti@gmail.com>
2026-09-18 13:20:57 +0500

    added state code for the graph

--- main.py
+++ main.py
@@ -13,7 +13,24 @@ from langchain_core.messages import (
 )
 
 from langchain_groq import ChatGroq
-
+from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
 
 from dotenv import load_dotenv
 load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# LLM
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile"
+)
+
+# State
+class TravelState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    user_query: str
+    flight_results: str
+    hotel_results: str
+    itinerary: str
+    llm_calls: int
