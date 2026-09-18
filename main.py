6c4eb94924d2e53fc8dd214a99a8e25e88d4da46
Abdul Rehman <dragonbhatti@gmail.com>
2026-09-18 13:23:47 +0500

    added hotel agent code that uses tavily tool for flight details

--- main.py
+++ main.py
@@ -46,3 +46,16 @@ def flight_agent(state: TravelState):
         ],
         "llm_calls": state.get("llm_calls", 0) + 1
     }
+
+# Hotel Agent
+def hotel_agent(state: TravelState):
+    query = f"Best hotels for {state['user_query']}"
+    hotel_results = tavily_search(query)
+
+    return {
+        "hotel_results": hotel_results,
+        "messages": [
+            AIMessage(content="Hotel information fetched")
+        ],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
