523227d07c6eb7834bdb228420b5121a13d28108
Abdul Rehman <dragonbhatti@gmail.com>
2026-09-19 13:11:29 +0500

    Created itinerary agent that creates schedule from all the data

--- main.py
+++ main.py
@@ -59,3 +59,30 @@ def hotel_agent(state: TravelState):
         ],
         "llm_calls": state.get("llm_calls", 0) + 1
     }
+
+def itinerary_agent(state: TravelState):
+
+    prompt = f"""
+    Create a travel itinerary.
+    User Query:
+    {state['user_query']}
+
+    Flight Results:
+    {state['flight_results']}
+
+    Hotel Results:
+    {state['hotel_results']}
+    """
+
+    response = llm.invoke([
+        SystemMessage(
+            content="You are an expert travel planner"
+        ),
+        HumanMessage(content=prompt)
+    ])
+
+    return {
+        "itinerary": response.content,
+        "messages": [response],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
